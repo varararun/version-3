69fe3742a986958e58dd6824654ef789cc2a595b
avarghese <arun.varghese@masergy.com>
2024-03-18 12:20:25 -0500

    fix manifest

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 const dataCacheName = 'avarghese-pwa';
 const cacheName = 'avarghese-pwa';
 const filesToCache = [
-    '/index.html'
+    '/',
+    'index.html',
+    'manifest.json',
+    'favicon.ico',
+    'assets/css/bundled.min.css',
+    'assets/js/bundled.min.js',
+    'assets/img/512.png'
 ];
 
 //install the sw
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
@@ -16,11 +22,11 @@ self.addEventListener('install', function (e) {
 });
 
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
                 if (key !== cacheName && key !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
@@ -32,10 +38,10 @@ self.addEventListener('activate', function (e) {
 });
 
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
     console.log('[Service Worker] Fetch', e.request.url);
     e.respondWith(
-        caches.match(e.request).then(function (response) {
+        caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
         })
     );
